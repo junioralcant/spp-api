f29d3b68939cd6e93ed768effd5d69edd1979770
Junior Marques <junioralcant111@gmail.com>
2020-01-16 09:55:08 -0300

    Ordenando listagem por createdAt de Nota Posto

--- src/app/controllers/NotaPostoController.js
+++ src/app/controllers/NotaPostoController.js
@@ -36,7 +36,7 @@ class NotaPostoController {
     const notaPostos = await NotaPosto.paginate(filters, {
       page: req.query.page || 1,
       limit: parseInt(req.query.limit_page) || 15,
-      sort: "-data",
+      sort: "-createdAt",
       populate: ["posto", "veiculo", "motorista"]
     });
 
