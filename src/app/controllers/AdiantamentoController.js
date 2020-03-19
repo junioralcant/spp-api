06ce77466e6cbebe2d54e4db088ef2605bf44999
Junior Marques <junioralcant111@gmail.com>
2020-03-19 15:24:34 -0300

    Criacao do controller de adiantamento

---
+++ src/app/controllers/AdiantamentoController.js
@@ -0,0 +1,91 @@
+const { formatToTimeZone } = require("date-fns-timezone");
+const Adiantamento = require("../models/Adiantamento");
+const Funcionario = require("../models/Funcionario");
+const Linha = require("../models/Linha");
+
+class AdiantamentoController {
+  async index(req, res) {
+    const filters = {};
+
+    if (req.query.nome_funcionario) {
+      filters.nomeFuncionario = new RegExp(req.query.nome_funcionario, "i");
+    }
+
+    if (req.query.data_min || req.query.data_max) {
+      filters.data = {};
+
+      const dataMinFormatada = formatToTimeZone(
+        req.query.data_min,
+        "YYYY-MM-DDT00:mm:ss.SSSZ", // formatação de data e hora
+        {
+          timeZone: "Europe/Berlin"
+        }
+      );
+
+      const dataMaxFormatada = formatToTimeZone(
+        req.query.data_max,
+        "YYYY-MM-DDT23:59:ss.SSSZ", // formatação de data e hora
+        {
+          timeZone: "Europe/Berlin"
+        }
+      );
+
+      filters.data.$gte = dataMinFormatada;
+      filters.data.$lte = dataMaxFormatada;
+    }
+
+    const adiantamentos = await Adiantamento.paginate(filters, {
+      page: req.query.page || 1,
+      limit: parseInt(req.query.limit_page) || 15,
+      sort: "-data",
+      populate: ["funcionario", "linha"]
+    });
+
+    return res.json(adiantamentos);
+  }
+
+  async store(req, res) {
+    const funcionario = await Funcionario.findById(req.body.funcionario);
+
+    const adiantamento = await Adiantamento.create({
+      ...req.body,
+      nomeFuncionario: funcionario.nome
+    });
+
+    return res.json(adiantamento);
+  }
+
+  async show(req, res) {
+    const adiantamento = await Adiantamento.findById(req.params.id).populate([
+      "funcionario",
+      "linha"
+    ]);
+
+    return res.json(adiantamento);
+  }
+
+  async update(req, res) {
+    const funcionario = await Funcionario.findById(req.body.funcionario);
+
+    const adiantamento = await Adiantamento.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true
+      }
+    );
+
+    adiantamento.nomeFuncionario = funcionario.nome;
+    await adiantamento.save();
+
+    return res.json(adiantamento);
+  }
+
+  async destroy(req, res) {
+    await Adiantamento.findByIdAndDelete(req.params.id);
+
+    return res.send();
+  }
+}
+
+module.exports = new AdiantamentoController();
